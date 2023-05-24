f78f5d55dfdf630ff24a55904c83cad26a038cc4
CrazikMoon <mrkicker525@gmail.com>
2023-05-24 18:59:49 +0400

    Первый коммит, долбавил файлы со старой незаконченной проги

---
+++ TabloProject/wording.py
@@ -0,0 +1,74 @@
+
+def f(x):
+    match x:
+        case 'A':
+            f=open("A.txt")
+            a=str(f)
+            print(a)
+            f.close()
+        case 'B':
+            return "B.txt"
+        case 'C':
+            return "C.txt"
+        case 'D':
+            return "D.txt"
+        case 'E':
+            return "E.txt"
+        case 'F':
+            return "F.txt"
+        case 'G':
+            return "G.txt"
+        case 'H':
+            return "H.txt"
+        case 'I':
+            return "I.txt"
+        case 'J':
+            return "J.txt"
+        case 'K':
+            return "K.txt"
+        case 'L':
+            return "L.txt"
+        case 'M':
+            return "M.txt"
+        case 'N':
+            return "N.txt"
+        case 'O':
+            return "O.txt"
+        case 'P':
+            return "P.txt"
+        case 'Q':
+            return "Q.txt"
+        case 'R':
+            return "R.txt"
+        case 'S':
+            return "S.txt"
+        case 'T':
+            return "T.txt"
+        case 'U':
+            return "U.txt"
+        case 'V':
+            return "V.txt"
+        case 'W':
+            return "W.txt"
+        case 'X':
+            return "X.txt"
+        case 'Y':
+            return "Y.txt"
+        case 'Z':
+            return "Z.txt"
+x=str(input())
+for i in range(0,len(x)):
+    f(x[i])
+    
+    
+    
+        
+
+
+
+
+
+
+        
+        
+        
